ce98764602a9880975cb89d0c6339eba86a2f6d1
yutaro.katayama <yuutarou.k@gmail.com>
2021-08-10 23:22:11 +0900

    getAllMessagesを追加

--- src/models/Tasks.java
+++ src/models/Tasks.java
@@ -7,11 +7,19 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 
 @Entity
+@NamedQueries({
+    @NamedQuery(
+        name = "getAllTasks",
+        query = "SELECT m FROM Tasks AS m ORDER BY m.id DESC"
+    )
+})
 @Table(name = "tasks")
-public class Message {
+public class Tasks {
     @Id
     @Column(name = "id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
